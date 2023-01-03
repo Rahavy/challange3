7e5ef1ec748ad3b9676d86ed3bb39a20fcd1e64b
Rahavy <114474771+Rahavy@users.noreply.github.com>
2023-01-03 21:25:38 +0000

    w

--- challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -35,7 +35,7 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.UpArrow) && !gameOver)
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
